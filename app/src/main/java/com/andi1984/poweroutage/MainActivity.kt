a0c433a7ef5c3713e94f305d82acdc8abf3ea202
Andreas Sander <mail@andi1984.de>
2022-02-22 22:29:32 +0100

    feat: Basic app logic workflow
    With this we provide the basic logical workflow of this app.

--- app/src/main/java/com/andi1984/poweroutage/MainActivity.kt
+++ app/src/main/java/com/andi1984/poweroutage/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.andi1984.poweroutage
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Bundle
+import android.telephony.PhoneNumberUtils
+import android.telephony.SmsManager
+import android.widget.EditText
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-
 class MainActivity : AppCompatActivity() {
+    val MESSAGE_ONLINE = "Strom ist wieder da."
+    val MESSAGE_OFFLINE = "Es gab einen Stromausfall!"
 
     private fun getBatteryStatus():Int? {
         val intent =
@@ -22,15 +28,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun powerOffEvent() {
-        //TODO: Send SMS that power went off
-
+        // Send SMS that power went off
         println("POWER OFF")
+
+        // Send SMS that power went on
+        debuggerWorkflow(MESSAGE_OFFLINE)
+        sendSMSWorkflow(MESSAGE_OFFLINE)
     }
 
     private fun powerOnEvent() {
-        // TODO: Send SMS that power went on
-
         println("POWER ON")
+
+        // Send SMS that power went on
+        debuggerWorkflow(MESSAGE_ONLINE)
+        sendSMSWorkflow(MESSAGE_ONLINE)
+    }
+
+    private fun debuggerWorkflow(message: String) {
+        // Check whether checkbox is enabled
+        val debuggerSwitch = findViewById<Switch>(R.id.debugger_switch)
+
+        if(!!debuggerSwitch.isChecked) {
+
+            // TODO: Show message in the UI
+            println(message)
+        }
+    }
+
+    private fun sendSMS(phoneNumber: String, message: String) {
+        val sentPI: PendingIntent = PendingIntent.getBroadcast(this, 0, Intent("SMS_SENT"), 0)
+        SmsManager.getDefault().sendTextMessage(phoneNumber, null, message, sentPI, null)
+    }
+
+    private fun sendSMSWorkflow(message: String) {
+        // Check whether checkbox is enabled
+        val smsSwitch = findViewById<Switch>(R.id.sms_switch)
+
+        // Get phone1, phone2
+        val primaryPhoneNumber = findViewById<EditText>(R.id.phone1)
+        val secondaryPhoneNumber = findViewById<EditText>(R.id.phone2)
+
+        if(smsSwitch.isChecked) {
+            println("Send SMS!!!")
+            // Send SMS to both
+            if(PhoneNumberUtils.isGlobalPhoneNumber(primaryPhoneNumber.text.toString())) {
+                sendSMS(primaryPhoneNumber.text.toString(), message)
+            }
+
+            if(PhoneNumberUtils.isGlobalPhoneNumber(secondaryPhoneNumber.text.toString())) {
+                sendSMS(secondaryPhoneNumber.text.toString(), message)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
